8985498b3b4af26b08f124b1085e229ed92724f9
2044smile <2044smile@naver.com>
2019-12-15 15:38:03 +0900

    init home, product APP

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('post.urls'))
+    path('', include('home.urls')),
+    path('product', include('product.urls')),
 ]
